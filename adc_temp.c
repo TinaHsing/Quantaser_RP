1cd308d1f87fa0a2a541f6843f8db6977c4c8adc
adam1025 <adam@quantaser.com>
2018-07-20 20:32:47 +0800

    rr

--- adc_temp.c
+++ adc_temp.c
@@ -21,38 +21,38 @@ int main(int argc, char **argv){
         rp_GenOutEnable(RP_CH_1);
 
 
-        uint32_t buff_size = 16384;
-        float *buff = (float *)malloc(buff_size * sizeof(float));
+        // uint32_t buff_size = 16384;
+        // float *buff = (float *)malloc(buff_size * sizeof(float));
 
-        rp_AcqReset();
-        rp_AcqSetDecimation(1);
-        rp_AcqSetTriggerLevel(RP_CH_1, 0.1); //Trig level is set in Volts while in SCPI
-        rp_AcqSetTriggerDelay(0);
+        // rp_AcqReset();
+        // rp_AcqSetDecimation(1);
+        // rp_AcqSetTriggerLevel(RP_CH_1, 0.1); //Trig level is set in Volts while in SCPI
+        // rp_AcqSetTriggerDelay(0);
 
-        rp_AcqStart();
+        // rp_AcqStart();
 
-        /* After acquisition is started some time delay is needed in order to acquire fresh samples in to buffer*/
-        /* Here we have used time delay of one second but you can calculate exact value taking in to account buffer*/
-        /*length and smaling rate*/
+        // /* After acquisition is started some time delay is needed in order to acquire fresh samples in to buffer*/
+        // /* Here we have used time delay of one second but you can calculate exact value taking in to account buffer*/
+        // /*length and smaling rate*/
 
-        sleep(1);
-        rp_AcqSetTriggerSrc(RP_TRIG_SRC_CHA_PE);
-        rp_acq_trig_state_t state = RP_TRIG_STATE_TRIGGERED;
+        // sleep(1);
+        // rp_AcqSetTriggerSrc(RP_TRIG_SRC_CHA_PE);
+        // rp_acq_trig_state_t state = RP_TRIG_STATE_TRIGGERED;
 
-        while(1){
-                rp_AcqGetTriggerState(&state);
-                if(state == RP_TRIG_STATE_TRIGGERED){
-                break;
-                }
-        }
+        // while(1){
+                // rp_AcqGetTriggerState(&state);
+                // if(state == RP_TRIG_STATE_TRIGGERED){
+                // break;
+                // }
+        // }
 
-        rp_AcqGetOldestDataV(RP_CH_1, &buff_size, buff);
-        int i;
-        for(i = 0; i < buff_size; i++){
-                printf("%f\n", buff[i]);
-        }
-        /* Releasing resources */
-        free(buff);
-        rp_Release();
-        return 0;
+        // rp_AcqGetOldestDataV(RP_CH_1, &buff_size, buff);
+        // int i;
+        // for(i = 0; i < buff_size; i++){
+                // printf("%f\n", buff[i]);
+        // }
+        // /* Releasing resources */
+        // free(buff);
+        // rp_Release();
+        // return 0;
 }
