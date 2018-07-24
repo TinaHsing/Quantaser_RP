1587dc103365a832aa45020bed62aa6aef38c1b9
adam1025 <adam@quantaser.com>
2018-07-24 22:40:44 +0800

    d

--- MS.c
+++ MS.c
@@ -79,7 +79,7 @@ int main(void)
 					{		
 									
 						t_temp[1] = micros() - t_temp[0];
-						printf("t_temp1: %ld\n",t_temp[1]);
+						printf("micros: %ld, temp[0]: %ld, temp[1]: %ld\n",micros(),t_temp[0],t_temp[1]);
 						if(t_temp[1] > tp*1000)
 						{
 							printf("1.t_now:%ld, amp=%f, dt=%ld\n",t_now,amp,t_temp[1]);
