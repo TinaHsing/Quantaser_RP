1268da545a213de556d61301ae5574e1d661b448
adam1025 <adam@quantaser.com>
2018-07-24 22:05:26 +0800

    d

--- test.c
+++ test.c
@@ -1,25 +1,58 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/time.h>
-//#include <windows.h>
 #include <stdint.h>
 #include <unistd.h>
-//#include "redpitaya/rp.h"
+#include "redpitaya/rp.h"
 //#include "C:\Users\Quantaser\Dropbox\projects\MassSpectrometer\rp.h"
 
 
 //global vars//
 /*1. function gen and ADC*/
-float f;
 
+int main(int argc, char **argv){
 
+	/* Print error, if rp_Init() function failed */
+	if(rp_Init() != RP_OK){
+		fprintf(stderr, "Rp api init failed!\n");
+	}
 
-int main(void)
-{
-	printf("input freq= \n");
-	scanf("%f",&f);
-	printf("u input :%f\n",f);
+	/* Generating frequency */
+	rp_GenFreq(RP_CH_1, 1000.0);
+
+	/* Generating amplitude */
+	rp_GenAmp(RP_CH_1, 0.5);
+
+	/* Generating wave form */
+	rp_GenWaveform(RP_CH_1, RP_WAVEFORM_SINE);
+
+	/* Enable channel */
+	rp_GenOutEnable(RP_CH_1);
+
+	/* Releasing resources */
+	rp_Release();
+	
+	sleep(1);
+	if(rp_Init() != RP_OK){
+		fprintf(stderr, "Rp api init failed!\n");
+	}
+
+	/* Generating frequency */
+	rp_GenFreq(RP_CH_1, 500.0);
+
+	/* Generating amplitude */
+	rp_GenAmp(RP_CH_1, 1);
+
+	/* Generating wave form */
+	rp_GenWaveform(RP_CH_1, RP_WAVEFORM_SINE);
+
+	/* Enable channel */
+	rp_GenOutEnable(RP_CH_1);
+
+	/* Releasing resources */
+	rp_Release();
+
 	return 0;
- } 
+}
  
 
