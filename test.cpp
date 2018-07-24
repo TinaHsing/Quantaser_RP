3cbe5d8981a08c169f718718544658f2d8e142bc
adam1025 <adam@quantaser.com>
2018-07-24 20:08:27 +0800

    g

---
+++ test.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/time.h>
+//#include <windows.h>
+#include <stdint.h>
+#include <unistd.h>
+//#include "redpitaya/rp.h"
+#include "C:\Users\Quantaser\Dropbox\projects\MassSpectrometer\rp.h"
+
+
+//global vars//
+/*1. function gen and ADC*/
+float f;
+
+
+
+int main(void)
+{
+	printf("input freq= \n");
+	scanf("%f",&f);
+	printf("u input :%f\n",f);
+	return 0;
+ } 
+ 
+
