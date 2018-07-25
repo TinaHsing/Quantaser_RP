cc2a791c3d591f280c2f37006a2614c4b1d75c07
adam1025 <adam@quantaser.com>
2018-07-25 19:59:51 +0800

    s

--- adc_temp.c
+++ adc_temp.c
@@ -10,7 +10,7 @@
 void ADC_req(uint32_t* , float* );
 long micros(void);
 int main(int argc, char **argv){
-		// long diff[64], time[2];
+		long diff[64], time[2];
         /* Print error, if rp_Init() function failed */
         if(rp_Init() != RP_OK){
                 fprintf(stderr, "Rp api init failed!\n");
@@ -52,25 +52,21 @@ int main(int argc, char **argv){
 
         // rp_AcqGetOldestDataV(RP_CH_1, &buff_size, buff);
 		
-		while(1)
-		{
-			// rp_AcqGetLatestDataV(RP_CH_1, &buff_size, buff);
-			// printf("%f, %f\n", buff[0],buff[buff_size-1]);
-		// for(int i=0; i<64; i++)
+		// while(1)
 		// {
-			// time[0]=micros();
+		for(int i=0; i<64; i++)
+		{
+			time[0]=micros();
 			ADC_req(&buff_size, buff);
-			// time[1]=micros();
-			// diff[i]=time[1]-time[0];
+			time[1]=micros();
+			diff[i]=time[1]-time[0];
 			
-		// }
-		// for(int i = 0; i < 64; i++){
-                // printf("%d, diff=%ld\n",i, diff[i]);
-        // }	
-        // for(i = 0; i < buff_size; i++){
-                // printf("%f\n", buff[i]);
-        // }
 		}
+		for(int i = 0; i < 64; i++)
+		{
+                printf("%d, diff=%ld\n",i, diff[i]);
+        }	
+		// }
         // /* Releasing resources */
         free(buff);
         rp_Release();
