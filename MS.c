ef0094c60c6b1e2a39d235dc0af03a7eb8decd03
adam1025 <adam@quantaser.com>
2018-07-27 17:02:02 +0800

    d

--- MS.c
+++ MS.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/time.h>
 //#include <windows.h>
 #include <stdint.h>
 #include <unistd.h>
+#include <fcntl.h> 
+#include <termios.h> 
 #include "redpitaya/rp.h"
 //#include "C:\Users\Quantaser\Dropbox\projects\MassSpectrometer\rp.h"
 
@@ -13,11 +16,17 @@ enum command{
 	DAC
 }com_sel;
 
+/*function gen and ADC*/
 long micros(void);
 void HVFG(float, float);
 void LVFG(float, float);
 void ADC_init(void);
 void ADC_req(uint32_t*, float*);
+/* UART */
+static int uart_init();
+static int release();
+static int uart_read(int size);
+static int uart_write();
 
 //global vars//
 /*1. function gen and ADC*/
@@ -25,7 +34,128 @@ float freq_HV;
 float a0_HV, a1_HV, a2_HV, a_LV;
 uint32_t t0_HV, t1_HV, t2_HV;
 long t_start, tp;
+/* UART */
+int uart_fd = -1;
 
+
+static int uart_init(){
+
+    uart_fd = open("/dev/ttyPS1", O_RDWR | O_NOCTTY | O_NDELAY);
+
+    if(uart_fd == -1){
+        fprintf(stderr, "Failed to open uart.\n");
+        return -1;
+    }
+
+    struct termios settings;
+    tcgetattr(uart_fd, &settings);
+
+    /*  CONFIGURE THE UART
+    *  The flags (defined in /usr/include/termios.h - see http://pubs.opengroup.org/onlinepubs/007908799/xsh/termios.h.html):
+    *       Baud rate:- B1200, B2400, B4800, B9600, B19200, B38400, B57600, B115200, B230400, B460800, B500000, B576000, B921600, B1000000, B1152000, B1500000, B2000000, B2500000, B3000000, B3500000, B4000000
+    *       CSIZE:- CS5, CS6, CS7, CS8
+    *       CLOCAL - Ignore modem status lines
+    *       CREAD - Enable receiver
+    *       IGNPAR = Ignore characters with parity errors
+    *       ICRNL - Map CR to NL on input (Use for ASCII comms where you want to auto correct end of line characters - don't use for bianry comms!)
+    *       PARENB - Parity enable
+    *       PARODD - Odd parity (else even) */
+
+    /* Set baud rate - default set to 9600Hz */
+    speed_t baud_rate = B9600;
+
+    /* Baud rate fuctions
+    * cfsetospeed - Set output speed
+    * cfsetispeed - Set input speed
+    * cfsetspeed  - Set both output and input speed */
+
+    cfsetspeed(&settings, baud_rate);
+
+    settings.c_cflag &= ~PARENB; /* no parity */
+    settings.c_cflag &= ~CSTOPB; /* 1 stop bit */
+    settings.c_cflag &= ~CSIZE;
+    settings.c_cflag |= CS8 | CLOCAL; /* 8 bits */
+    settings.c_lflag = ICANON; /* canonical mode */
+    settings.c_oflag &= ~OPOST; /* raw output */
+
+    /* Setting attributes */
+    tcflush(uart_fd, TCIFLUSH);
+    tcsetattr(uart_fd, TCSANOW, &settings);
+
+    return 0;
+}
+static int uart_read(int size){
+
+    /* Read some sample data from RX UART */
+
+    /* Don't block serial read */
+    fcntl(uart_fd, F_SETFL, FNDELAY);
+
+    while(1){
+        if(uart_fd == -1){
+            fprintf(stderr, "Failed to read from UART.\n");
+            return -1;
+        }
+
+        unsigned char rx_buffer[size];
+
+        int rx_length = read(uart_fd, (void*)rx_buffer, size);
+		// printf("length = %d\n", rx_length);
+        if (rx_length < 0){
+
+            /* No data yet avaliable, check again */
+            if(errno == EAGAIN){
+                // fprintf(stderr, "AGAIN!\n");
+                continue;
+            /* Error differs */
+            }else{
+                fprintf(stderr, "Error!\n");
+                return -1;
+            }
+
+        }else if (rx_length == 0){
+            fprintf(stderr, "No data waiting\n");
+        /* Print data and exit while loop */
+        }else{
+            rx_buffer[rx_length] = '\0';
+            printf("%i bytes read : %s\n", rx_length, rx_buffer);
+            break;
+
+        }
+    }
+
+    return 0;
+}
+static int uart_write(char *data){
+
+    /* Write some sample data into UART */
+    /* ----- TX BYTES ----- */
+    int msg_len = strlen(data);
+
+    int count = 0;
+    char tx_buffer[msg_len+1];
+
+    strncpy(tx_buffer, data, msg_len);
+    tx_buffer[msg_len++] = 0x0a; //New line numerical value
+
+    if(uart_fd != -1){
+        count = write(uart_fd, &tx_buffer, (msg_len));
+    }
+    if(count < 0){
+        fprintf(stderr, "UART TX error.\n");
+        return -1;
+    }
+
+    return 0;
+}
+
+static int release(){
+
+    tcflush(uart_fd, TCIFLUSH);
+    close(uart_fd);
+
+    return 0;
+}
 
 int main(void)
 {
@@ -37,7 +167,10 @@ int main(void)
 	/******ADC******/
 	uint32_t buff_size = 2;
     float *buff = (float *)malloc(buff_size * sizeof(float));
-	
+	/******UART******/
+	char uart_cmd[10];
+	char *size = "123456789";
+	int uart_return = 0;
 	
 		do
 		{
@@ -129,8 +262,30 @@ int main(void)
 				free(buff);
 				rp_Release();				
 			break;
-			case BBB:
-				printf("now in BBB\n");
+			case UART:
+				printf("--Selecting Function UART---\n");
+				if(uart_init() < 0)
+				{
+					printf("Uart init error.\n");
+					return -1;
+				}		
+				do
+				{
+					printf("enter command: \n");
+					scanf("%s", uart_cmd);
+					if(uart_write(uart_cmd) < 0){
+					printf("Uart write error\n");
+					return -1;
+					}
+		
+					if(uart_read(strlen(size)) < 0)
+					{
+						printf("Uart read error\n");
+						return -1;
+					}
+					printf("Exit uart? Yes:1, No:0\n");
+					scanf("%d",&uart_return);
+				}while(!uart_return);
 				
 			break;
 			default :
