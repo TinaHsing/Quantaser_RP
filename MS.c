930d3742084005288f37e726200579423f549abb
adam1025 <adam@quantaser.com>
2018-07-27 18:42:08 +0800

    d

--- MS.c
+++ MS.c
@@ -50,9 +50,9 @@ long t_start, tp;
 int uart_fd = -1;
 /* I2C */
 int g_i2cFile;
-// unsigned int i2c_com, i2c_data;
-uint8_t i2c_com;
-uint16_t i2c_data;
+unsigned int i2c_com, i2c_data;
+// uint8_t i2c_com;
+// uint16_t i2c_data;
 
 int main(void)
 {
