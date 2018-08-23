8453559746e6f6334aaa6c117c2de26b9484d32e
adam1025 <adam@quantaser.com>
2018-08-23 22:07:47 +0800

    d

---
+++ tina_fg.c
@@ -0,0 +1,38 @@
+/* Red Pitaya C API example Generating continuous signal  
+ * This application generates a specific signal */
+
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#include "redpitaya/rp.h"
+
+int main(int argc, char **argv){
+
+	/* Print error, if rp_Init() function failed */
+	if(rp_Init() != RP_OK){
+		fprintf(stderr, "Rp api init failed!\n");
+	}
+
+	/* Generating frequency */
+	rp_GenFreq(RP_CH_1, 300000.0);
+	rp_GenFreq(RP_CH_2, 200000.0);
+
+	/* Generating amplitude */
+	rp_GenAmp(RP_CH_1, 1);
+	rp_GenAmp(RP_CH_2, 1);
+
+	/* Generating wave form */
+	rp_GenWaveform(RP_CH_1, RP_WAVEFORM_SINE);
+	rp_GenWaveform(RP_CH_2, RP_WAVEFORM_SQUARE);
+
+	/* Enable channel */
+	rp_GenOutEnable(RP_CH_1);
+	rp_GenOutEnable(RP_CH_2);
+
+	/* Releasing resources */
+	rp_Release();
+
+	return 0;
+}
