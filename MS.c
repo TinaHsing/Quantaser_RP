fad57d5f442608ebf608c334798bb5263a805a0a
adam1025 <adam@quantaser.com>
2018-09-28 16:53:15 +0800

    d

--- MS.c
+++ MS.c
@@ -437,7 +437,7 @@ static int uart_init(){
     cfsetspeed(&settings, baud_rate);
 
     settings.c_cflag &= ~PARENB; /* no parity */
-    settings.c_cflag &= CSTOPB; /* 1 stop bit */
+    // settings.c_cflag &= ~CSTOPB; /* 1 stop bit */
     settings.c_cflag &= ~CSIZE;
     settings.c_cflag |= CS8 | CLOCAL; /* 8 bits */
     settings.c_lflag = ICANON; /* canonical mode */
