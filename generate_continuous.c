f62e550b6a2b6aa902fa5291843392ca2844668e
adam1025 <adam@quantaser.com>
2018-10-09 21:19:48 +0800

    d

--- generate_continuous.c
+++ generate_continuous.c
@@ -29,6 +29,10 @@
 #define FGTRIG 983
 
 long micros(void);
+static int pin_export(int);
+static int pin_unexport(int);
+static int pin_direction(int, int);
+static int pin_write(int, int);
 int main(int argc, char **argv){
 	float freq;
 	long t0, t_now;
@@ -95,4 +99,61 @@ long micros(){
 	// if(time<0) time = time*(-1);
 //	return currentTime.tv_sec * (int)1e6 + currentTime.tv_usec;
 	return time;
+}
+static int pin_export(int pin)
+{
+	char shell[MAX_PATH];
+	sprintf(shell,"echo %d > /sys/class/gpio/export", pin);
+	system(shell);
+	return 0;
+}
+
+static int pin_unexport(int pin)
+{
+        char shell[MAX_PATH];
+        sprintf(shell,"echo %d > /sys/class/gpio/unexport", pin);
+        system(shell);
+
+	return 0;
+}
+
+static int pin_direction(int pin, int dir){
+
+	char shell[MAX_PATH];
+	snprintf(shell, MAX_PATH, "echo %s > /sys/class/gpio/gpio%d/direction",((dir==IN)?"in":"out"),pin);
+	system(shell);
+
+	return 0;
+}
+
+static int pin_write(int pin, int value)
+{
+	char path[VALUE_MAX];
+	int fd;
+
+	snprintf(path, VALUE_MAX, "/sys/class/gpio/gpio%d/value", pin);
+	// get pin value file descrptor
+	fd = open(path, O_WRONLY);
+	if (-1 == fd) {
+		fprintf(stderr, "Unable to to open sysfs pins value file %s for writing\n",path);
+		return -1;
+	}
+	if(value==LOW){
+		//write low
+		if (1 != write(fd, "0", 1)) {
+			fprintf(stderr, "Unable to write value\n");
+			return -1;
+		}
+	}
+        else if(value==HIGH){
+		//write high
+		if (1 != write(fd, "1", 1)) {
+                	fprintf(stderr, "Unable to write value\n");
+                	return -1;
+		}
+	}else fprintf(stderr, "Nonvalid pin value requested\n");
+
+	//close file
+	close(fd);
+	return 0;
 }
