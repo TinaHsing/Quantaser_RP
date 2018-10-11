a2b4bf3c405364435aee6d2beb8357aae887dc3b
adam1025 <adam@quantaser.com>
2018-10-11 20:40:18 +0800

    d

--- arb_fg.c
+++ arb_fg.c
@@ -54,9 +54,9 @@ int main(int argc, char **argv){
     rp_GenOutEnable(RP_CH_1);
     // rp_GenOutEnable(RP_CH_2);
 	
-	while((micros()-t0)<sweep_time);
+	// while((micros()-t0)<sweep_time);
 	
-	rp_GenOutDisable(RP_CH_1);
+	// rp_GenOutDisable(RP_CH_1);
 	
 	// rp_GenArbWaveform(RP_CH_1, y, buff_size);
 	// t0 = micros();
