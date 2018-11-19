5f80972f629379fc5b3655049f6cd0447d76a617
adam1025 <adam@quantaser.com>
2018-11-19 16:40:54 +0800

    f

--- burst_test.c
+++ burst_test.c
@@ -37,7 +37,7 @@ int main(int argc, char **argv){
         rp_GenBurstRepetitions(RP_CH_1, rp);
         rp_GenBurstPeriod(RP_CH_1, period);
 		// rp_GenTrigger(1);
-        sleep(1);
+        // sleep(1);
         rp_GenOutEnable(RP_CH_1);
         // rp_Release();
 }
