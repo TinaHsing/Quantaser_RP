30c9bb4a431cd7ee86e3867eb0225ab72e5e3e80
adam1025 <adam@quantaser.com>
2018-11-27 17:39:19 +0800

    f

--- MS_v2.c
+++ MS_v2.c
@@ -487,7 +487,7 @@ int main(void)
 				
 				
 				t_start = micros();				
-				rp_GenWaveform(RP_CH_2, RP_WAVEFORM_ARBITRARY);
+				rp_GenWaveform(RP_CH_2, RP_WAVEFORM_SINE);//RP_WAVEFORM_ARBITRARY
 				printf("time0=%ld\n",micros()-t_start);
 				rp_GenFreq(RP_CH_2, 1000.0/sweep_time);
 //				while((micros()-t_start)<CHIRP_WAIT*1000){};
