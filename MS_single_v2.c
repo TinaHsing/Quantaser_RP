8e45d2dccc295e0fbff72aa8778c24bd96dd8e6b
adam1025 <adam@quantaser.com>
2018-12-24 16:53:59 +0800

    d

--- MS_single_v2.c
+++ MS_single_v2.c
@@ -200,7 +200,7 @@ int main(int argc, char *argv[])
 		// AddrWrite(0x40200004, 0);
 	}
 	
-	// printf("num=%d\n",num);
+	printf("num=%d\n",num);
 	amp = a2_HV;
 	rp_GenAmp(RP_CH_1, amp);
 	rp_GenAmp(RP_CH_2, 0);
