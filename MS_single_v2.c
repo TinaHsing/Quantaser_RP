761eaac8b9c37375afd56bbf819f636e2896a77c
adam1025 <adam@quantaser.com>
2019-01-22 20:27:42 +0800

    d

--- MS_single_v2.c
+++ MS_single_v2.c
@@ -356,7 +356,7 @@ void write_txt(float* adc_data, int save)
 		for(int i=0;i<idx;i++)
 		{
 			// printf("%d. %f\n",i+1, *(adc_data+i));
-			printf("%f\n", *(adc_data+i));
+			// printf("%f\n", *(adc_data+i));
 			sprintf(shell,"echo %f >> adc_data.txt", *(adc_data+i));
 			system(shell);
 		}
