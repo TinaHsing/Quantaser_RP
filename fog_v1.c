e7c7bfe81c5de9442adfb788b66212a3a8a29c2d
adam1025 <adam@quantaser.com>
2019-03-21 17:16:58 +0800

    dd

--- fog_v1.c
+++ fog_v1.c
@@ -73,63 +73,48 @@ int main(int argc, char *argv[])
 	return 0;
 }
 
-static int pin_export(int pin)
-{
-	char shell[MAX_PATH];
-	sprintf(shell,"echo %d > /sys/class/gpio/export", pin);
-	system(shell);
-	return 0;
-}
 
-static int pin_unexport(int pin)
-{
-        char shell[MAX_PATH];
-        sprintf(shell,"echo %d > /sys/class/gpio/unexport", pin);
-        system(shell);
 
-	return 0;
-}
+// static int pin_direction(int pin, int dir){
 
-static int pin_direction(int pin, int dir){
+	// char shell[MAX_PATH];
+	// snprintf(shell, MAX_PATH, "echo %s > /sys/class/gpio/gpio%d/direction",((dir==IN)?"in":"out"),pin);
+	// system(shell);
 
-	char shell[MAX_PATH];
-	snprintf(shell, MAX_PATH, "echo %s > /sys/class/gpio/gpio%d/direction",((dir==IN)?"in":"out"),pin);
-	system(shell);
+	// return 0;
+// }
 
-	return 0;
-}
+// static int pin_write(int pin, int value)
+// {
+	// char path[VALUE_MAX];
+	// int fd;
 
-static int pin_write(int pin, int value)
-{
-	char path[VALUE_MAX];
-	int fd;
+	// snprintf(path, VALUE_MAX, "/sys/class/gpio/gpio%d/value", pin);
+	get pin value file descrptor
+	// fd = open(path, O_WRONLY);
+	// if (-1 == fd) {
+		// fprintf(stderr, "Unable to to open sysfs pins value file %s for writing\n",path);
+		// return -1;
+	// }
+	// if(value==LOW){
+		write low
+		// if (1 != write(fd, "0", 1)) {
+			// fprintf(stderr, "Unable to write value\n");
+			// return -1;
+		// }
+	// }
+        // else if(value==HIGH){
+		write high
+		// if (1 != write(fd, "1", 1)) {
+                	// fprintf(stderr, "Unable to write value\n");
+                	// return -1;
+		// }
+	// }else fprintf(stderr, "Nonvalid pin value requested\n");
 
-	snprintf(path, VALUE_MAX, "/sys/class/gpio/gpio%d/value", pin);
-	// get pin value file descrptor
-	fd = open(path, O_WRONLY);
-	if (-1 == fd) {
-		fprintf(stderr, "Unable to to open sysfs pins value file %s for writing\n",path);
-		return -1;
-	}
-	if(value==LOW){
-		//write low
-		if (1 != write(fd, "0", 1)) {
-			fprintf(stderr, "Unable to write value\n");
-			return -1;
-		}
-	}
-        else if(value==HIGH){
-		//write high
-		if (1 != write(fd, "1", 1)) {
-                	fprintf(stderr, "Unable to write value\n");
-                	return -1;
-		}
-	}else fprintf(stderr, "Nonvalid pin value requested\n");
-
-	//close file
-	close(fd);
-	return 0;
-}
+	close file
+	// close(fd);
+	// return 0;
+// }
 static int uart_init(){
 
     uart_fd = open("/dev/ttyPS1", O_RDWR | O_NOCTTY | O_NDELAY);
