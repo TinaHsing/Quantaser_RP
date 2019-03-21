fa780ff3187671d8064dd5bb4be431d45f88864c
adam1025 <adam@quantaser.com>
2019-03-21 21:27:55 +0800

    c

--- fog_v1.c
+++ fog_v1.c
@@ -76,9 +76,9 @@ int main(int argc, char *argv[])
 		printf("cmd= %d\n",command[0]);
 		
 		
-		if(command[0] == 48)
+		if(command[0] == '0')
 			printf("it's 0!\n");
-		else if(command[0] == 49)
+		else if(command[0] == '1')
 			printf("it's 1!\n");
 		else 
 			printf("nonono\n");
