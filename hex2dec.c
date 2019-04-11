57f09113b7c0c530c3e1b7141425c3318ef37b34
adam1025 <adam@quantaser.com>
2019-04-11 16:54:20 +0800

    d

--- hex2dec.c
+++ hex2dec.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
-char data[8];
+// char data[8];
 int main(int argc, char *argv[])
 {
-	data = argv[1];
-	printf("%s\n", data);
+	// data = argv[1];
+	printf("%s\n", argv[1]);
 	return 0;
 }
