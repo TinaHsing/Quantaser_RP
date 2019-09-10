a8b404893d41c32b028ba87fe5275153b1c93ec2
adam1025 <adam@quantaser.com>
2019-09-10 17:52:32 +0800

    d

--- adc.c
+++ adc.c
@@ -51,7 +51,7 @@ int main(int argc, char *argv[]){
 	rp_AcqSetTriggerLevel(trig_src, trig_level);
 	rp_AcqSetTriggerDelay(0);
 	rp_AcqStart();
-	
+	sleep(1);
 	if(trig_src == RP_CH_1) 
 		rp_AcqSetTriggerSrc(RP_TRIG_SRC_CHA_PE);
 	else if(trig_src == RP_CH_2)
