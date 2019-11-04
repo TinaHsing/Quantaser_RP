540e46917ceb4ea20b60d6e12f037cafb4d738b0
adam1025 <adam@quantaser.com>
2019-11-04 18:09:59 +0800

    f

--- fog_v1.c
+++ fog_v1.c
@@ -269,7 +269,7 @@ static int uart_read(int size){
         /* Print data and exit while loop */
         }else{
             rx_buffer[rx_length] = '\0';
-            // printf("%i bytes read : %s\n", rx_length, rx_buffer);
+            printf("%i bytes read : %s\n", rx_length, rx_buffer);
 			// printf("%s\n", rx_buffer);
 			command[0] = rx_buffer[0];
             break;
