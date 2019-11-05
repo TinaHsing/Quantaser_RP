2786e730c1fc0b6dd499fc2bf374df8844a95597
adam1025 <adam@quantaser.com>
2019-11-05 16:53:02 +0800

    d

--- fog_v1.c
+++ fog_v1.c
@@ -90,7 +90,7 @@ int main(int argc, char *argv[])
 			}
 			#else
 				sprintf(data,"%d", AddrRead(address));
-				printf("%d\n", (*int)data);
+				printf("%d\n", (int)data);
 				// sprintf(data,"%d", (int)65535);
 				uart_write(data);
 			#endif
