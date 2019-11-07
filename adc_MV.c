3f512fe309967cb45a449a37035fe43ef531c44c
adam1025 <adam@quantaser.com>
2019-11-07 16:10:24 +0800

    d

--- adc_MV.c
+++ adc_MV.c
@@ -12,7 +12,7 @@ int main(int argc, char **argv){
                 fprintf(stderr, "Rp api init failed!\n");
         }
 
-		uint32_t buff_size;
+		uint32_t buff_size, hold_times;
 		int ch, gain;
         float *buff;
 
@@ -20,6 +20,7 @@ int main(int argc, char **argv){
 		buff_size = atoi(argv[2]);
 		buff = (float *)malloc(buff_size * sizeof(float));
 		gain = atoi(argv[3]);
+		hold_times = atoi(argv[2]);
 		
         rp_AcqReset();
 		rp_AcqSetDecimation(1);
@@ -31,7 +32,7 @@ int main(int argc, char **argv){
 		// for(int i=0; i<64; i++)
 		// {
 			// time[0]=micros();
-			ADC_req(&buff_size, buff, ch);
+			ADC_req(&buff_size, buff, ch, hold_times);
 			// time[1]=micros();
 			// diff[i]=time[1]-time[0];
 			// printf("%ld\n", time[1]-time[0]);
@@ -52,14 +53,18 @@ int main(int argc, char **argv){
 //	return currentTime.tv_sec * (int)1e6 + currentTime.tv_usec;
 	return time;
 }
-void ADC_req(uint32_t* buff_size, float* buff, int ch) {
+void ADC_req(uint32_t* buff_size, float* buff, int ch, uint32_t hold_times) {
 	rp_AcqGetLatestDataV(ch, buff_size, buff);
-	int i;
-	float avg = 0;
+	int i, j;
+	float avg = 0, sum = 0;
+	for(j=0; j<hold_times; j++) {
         for(i = 0; i < *buff_size; i++){
                 // printf("%f\n", buff[i]);
 				avg += buff[i];
         }
-	avg = avg / (float)*buff_size;
-	printf("%f\n", avg);
+		avg = avg / (float)*buff_size;
+		sum += avg;
+		printf("%f\n", avg);
+	}
+	printf("%f\n", sum);
 }
