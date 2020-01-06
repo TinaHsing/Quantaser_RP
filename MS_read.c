eeb6d492b7efb00393dfdb7e54124e8de06a1633
adam1025 <adam@quantaser.com>
2020-01-06 13:10:22 +0800

    d

--- MS_read.c
+++ MS_read.c
@@ -315,9 +315,9 @@ int main(int argc, char *argv[])
 	{
 		AddrWrite(0x40200064, i);//addwrite idx 
 		// printf("idx=%d, ",AddrRead(0x40200064));
-		adc_mem[i] = AddrRead(0x40200070); //read fpga adc_mem[idx], 0x40200068 for ch1, 0x40200070 for ch2
+		adc_mem[i] = AddrRead(0x40200068); //read fpga adc_mem[idx], 0x40200068 for ch1, 0x40200070 for ch2
 		adc_mem_f[i] = int2float(*(adc_mem+i), adc_gain_p, adc_gain_n, adc_offset);
-		printf("adc_mem[%d]=%d\n",i, adc_mem[i]);
+		// printf("adc_mem[%d]=%d\n",i, adc_mem[i]);
 	}
 	AddrWrite(0x4020005C, 1); //end read flag, reset adc_counter
 	
