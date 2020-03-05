f0e2de33abd0b7e4b24527a47f35e7019007bb20
adam1025 <adam@quantaser.com>
2020-03-05 16:59:26 +0800

    f

--- arb_fg.c
+++ arb_fg.c
@@ -31,19 +31,19 @@ void AddrWrite(unsigned long, unsigned long);
 int main(int argc, char **argv){
 
     float sweep_time_1, sweep_time_2;
-	long arb_size = 16384;
+	long arb_size = 800000;
 	// long t_start;
 	float start_freq_1, final_freq_1, k_1;
-	float start_freq_2, final_freq_2, k_2;
+	// float start_freq_2, final_freq_2, k_2;
 	// int out[arb_size];
 	int out;
-	
+	float t, x;
 	start_freq_1 = atof(argv[1]);
 	final_freq_1 = atof(argv[2]);
 	sweep_time_1 = atof(argv[3]);
-	start_freq_2 = atof(argv[4]);
-	final_freq_2 = atof(argv[5]);
-	sweep_time_2 = atof(argv[6]);
+	// start_freq_2 = atof(argv[4]);
+	// final_freq_2 = atof(argv[5]);
+	// sweep_time_2 = atof(argv[6]);
 	
     /* Print error, if rp_Init() function failed */
     if(rp_Init() != RP_OK){
@@ -52,19 +52,21 @@ int main(int argc, char **argv){
 	// rp_GenAmp(RP_CH_2, 1);
 	// rp_GenOutEnable(RP_CH_2);
 	AddrWrite(0x40200024, 8192);
-    float *t = (float *)malloc(arb_size * sizeof(float));
-	float *x_1 = (float *)malloc(arb_size * sizeof(float));
-	float *t2 = (float *)malloc(arb_size * sizeof(float));
-	float *x_2 = (float *)malloc(arb_size * sizeof(float));
+    // float *t = (float *)malloc(arb_size * sizeof(float));
+	// float *x_1 = (float *)malloc(arb_size * sizeof(float));
+	// float *t2 = (float *)malloc(arb_size * sizeof(float));
+	// float *x_2 = (float *)malloc(arb_size * sizeof(float));
 	k_1 = (final_freq_1 - start_freq_1) / sweep_time_1;
-	k_2 = (final_freq_2 - start_freq_2) / sweep_time_2;
+	// k_2 = (final_freq_2 - start_freq_2) / sweep_time_2;
 	for(long i = 0; i < arb_size; i++){
-		t[i] = (float)sweep_time_1 / arb_size * i;
-		x_1[i] = sin(2*M_PI*(start_freq_1*t[i] + 0.5*k_1*t[i]*t[i]));
-		t2[i] = (float)sweep_time_2 / arb_size * i;
-		x_2[i] = sin(2*M_PI*(start_freq_2*t2[i] + 0.5*k_2*t2[i]*t2[i]));
+		// t[i] = (float)sweep_time_1 / arb_size * i;
+		// x_1[i] = sin(2*M_PI*(start_freq_1*t[i] + 0.5*k_1*t[i]*t[i]));
+		// t2[i] = (float)sweep_time_2 / arb_size * i;
+		// x_2[i] = sin(2*M_PI*(start_freq_2*t2[i] + 0.5*k_2*t2[i]*t2[i]));
 		// out[i] = x_1[i]*8191; 
-		out = (int)(x_1[i]*8191.0);
+		t = (float)sweep_time_1 / arb_size * i;
+		x = sin(2*M_PI*(start_freq_1*t + 0.5*k_1*t*t));
+		out = (int)(x*8191.0);
 		AddrWrite(0x40200080, out);
 	}
 	// rp_GenAmp(RP_CH_2, 0);
