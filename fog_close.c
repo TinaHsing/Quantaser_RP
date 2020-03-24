b4811e99126b6fed273328af0422a22a190c8690
adam1025 <adam@quantaser.com>
2020-03-24 16:19:49 +0800

    d

--- fog_close.c
+++ fog_close.c
@@ -76,7 +76,7 @@ uint32_t data_addr = 0x400001E0;
 uint32_t data_int[DATA_SIZE];
 int data_in, data_in_2;
 FILE *fp;
-
+char ii = 0;
 int main(int argc, char *argv[])
 {
 	#ifdef CONTINUE
@@ -107,7 +107,8 @@ int main(int argc, char *argv[])
 				///////for kalmman out///////////
 				sprintf(data,"%d", AddrRead(address));
 				//////////////////////////////////
-				uart_write(data);
+				uart_write(ii);
+				ii++;
 				uart_read(10);
 				usleep(SEND_DELAY_us);
 			
