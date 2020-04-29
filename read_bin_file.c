504ecaecc6c2988580378befb080b3b83cb85acf
adam1025 <adam@quantaser.com>
2020-04-29 16:47:19 +0800

    f

--- read_bin_file.c
+++ read_bin_file.c
@@ -5,7 +5,7 @@ int main(int argc, char *argv[])
 {
 	FILE *fp;
 	size_t cnt = atoi(argv[2]);
-	double arr[50000];
+	float arr[50000];
 	
 	fp = fopen(argv[1], "rb");
 	fread(arr, sizeof(double), cnt, fp);
