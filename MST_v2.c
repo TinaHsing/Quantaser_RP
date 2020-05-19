ba67627b4fd7428c98376fbedd935bf8375449a7
adam1025 <adam@quantaser.com>
2020-05-19 15:51:43 +0800

    d

--- MST_v2.c
+++ MST_v2.c
@@ -244,7 +244,7 @@ void AddrCpy(uint32_t addr, uint32_t* arr)
 	virt_addr = map_base + (addr & MAP_MASK);
 	
 	// *((uint32_t *) virt_addr) = *arr;
-	memcpy(arr, (uint32_t *) virt_addr, 4);
+	memcpy(arr, (uint32_t *) virt_addr, 1);
 	
 	if (map_base != (void*)(-1)) {
 		if(munmap(map_base, MAP_SIZE) == -1) FATAL;
