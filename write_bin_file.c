01ed964000642f6fe6b70773246af384f0386135
adam1025 <adam@quantaser.com>
2020-05-20 15:22:15 +0800

    d

--- write_bin_file.c
+++ write_bin_file.c
@@ -1,7 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <sys/time.h>
+#include <sys/ioctl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <linux/ioctl.h>
+#include <linux/i2c-dev.h>
+#include <stdint.h>
+#include <unistd.h>
+#include <fcntl.h> 
+#include <termios.h> 
+#include <errno.h>
 #include <math.h>
+#include <signal.h>
 #include <ctype.h>
+#include <sys/mman.h>
+#include "redpitaya/rp.h"
 
 #define CHIRP_SWEEP_TIME 8.0
 
