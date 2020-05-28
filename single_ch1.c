838e2b1a3ec264666277bc55e9c753ee5c5bdccd
adam1025 <adam@quantaser.com>
2020-05-28 13:46:33 +0800

    d

--- single_ch1.c
+++ single_ch1.c
@@ -13,23 +13,20 @@ int main(int argc, char *argv[]){
     if(rp_Init() != RP_OK){
         fprintf(stderr, "Rp api init failed!\n");
     }
-	// printf("enter frequency(Hz) and amplitude(0~1V)\n");
-	// scanf("%f%f",&freq,&amp);
-	// while ( getchar() != '\n' );
-	// rp_GenAmp(RP_CH_2, 0);
-	// freq = atof(argv[1])*1000; //KHz
-	// amp = atof(argv[2])/1000;
-	// offset = atof(argv[3])/1000;
-	// rp_GenWaveform(RP_CH_2, RP_WAVEFORM_SINE);
-    // rp_GenFreq(RP_CH_2, freq);
+	rp_GenAmp(RP_CH_2, 0);
+	freq = atof(argv[1])*1000; //KHz
+	amp = atof(argv[2])/1000;
+	offset = atof(argv[3])/1000;
+	rp_GenWaveform(RP_CH_2, RP_WAVEFORM_SINE);
+    rp_GenFreq(RP_CH_2, freq);
 	
-	// rp_GenOffset(RP_CH_2, offset);
+	rp_GenOffset(RP_CH_2, offset);
 
-    // rp_GenAmp(RP_CH_2, amp);
+    rp_GenAmp(RP_CH_2, amp);
 
     
 
-    // rp_GenOutEnable(RP_CH_2);
+    rp_GenOutEnable(RP_CH_2);
 
     rp_Release();
 
