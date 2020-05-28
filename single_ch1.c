5649a4beae5244d103efd3c23da22e9a100639a7
adam1025 <adam@quantaser.com>
2020-05-28 13:20:42 +0800

    s

--- single_ch1.c
+++ single_ch1.c
@@ -16,25 +16,20 @@ int main(int argc, char *argv[]){
 	// printf("enter frequency(Hz) and amplitude(0~1V)\n");
 	// scanf("%f%f",&freq,&amp);
 	// while ( getchar() != '\n' );
-	rp_GenAmp(RP_CH_2, 0);
-	freq = atof(argv[1])*1000; //KHz
-	amp = atof(argv[2])/1000;
-	offset = atof(argv[3])/1000;
-    /* Generating frequency */
-    rp_GenFreq(RP_CH_2, freq);
+	// rp_GenAmp(RP_CH_2, 0);
+	// freq = atof(argv[1])*1000; //KHz
+	// amp = atof(argv[2])/1000;
+	// offset = atof(argv[3])/1000;
+    // rp_GenFreq(RP_CH_2, freq);
 	
-	rp_GenOffset(RP_CH_2, offset);
+	// rp_GenOffset(RP_CH_2, offset);
 
-    /* Generating amplitude */
-    rp_GenAmp(RP_CH_2, amp);
+    // rp_GenAmp(RP_CH_2, amp);
 
-    /* Generating wave form */
-    rp_GenWaveform(RP_CH_2, RP_WAVEFORM_SINE);
+    // rp_GenWaveform(RP_CH_2, RP_WAVEFORM_SINE);
 
-    /* Enable channel */
-    rp_GenOutEnable(RP_CH_2);
+    // rp_GenOutEnable(RP_CH_2);
 
-    /* Releasing resources */
     rp_Release();
 
     return 0;
