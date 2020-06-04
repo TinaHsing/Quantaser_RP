f75ad11a59bcd130cf8edc6e407ab297f869d96a
adam1025 <adam@quantaser.com>
2020-06-04 12:52:47 +0800

    d

--- MS_single_v5.c
+++ MS_single_v5.c
@@ -264,8 +264,8 @@ int main(int argc, char *argv[])
 		trapping_temp = trapping_amp;
 		for(int i=0; i<ramp_pts; i++) 
 		{		
-			AddrWrite(0x40200064, i);//addwrite idx
-			// *adc_idx_addr = i;//addwrite idx
+			// AddrWrite(0x40200064, i);//addwrite idx
+			*adc_idx_addr = i;//addwrite idx
 			trapping_temp += ramp_step;
 			ramp_ch2 += ramp_step2;
 			while((micros() - t_start) < UPDATE_RATE){}; 	
@@ -286,10 +286,10 @@ int main(int argc, char *argv[])
 		
 		for(int i=0; i<adc_counter; i++)
 		{
-			AddrWrite(0x40200064, i);//addwrite idx 
-			// *adc_idx_addr = i;//addwrite idx
-			adc_mem[i] = AddrRead(0x40200070); //read fpga adc_mem[idx], 0x40200068 for ch1, 0x40200070 for ch2
-			// adc_mem[i] = *adc_ch2;
+			// AddrWrite(0x40200064, i);//addwrite idx 
+			*adc_idx_addr = i;//addwrite idx
+			// adc_mem[i] = AddrRead(0x40200070); //read fpga adc_mem[idx], 0x40200068 for ch1, 0x40200070 for ch2
+			adc_mem[i] = *adc_ch2;
 			adc_mem_f[i] = int2float(*(adc_mem+i), adc_gain_p, adc_gain_n, adc_offset);
 		}
 		AddrWrite(0x4020005C, 1); //end read flag, reset adc_counter
