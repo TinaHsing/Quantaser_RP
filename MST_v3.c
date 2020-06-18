27cfdad17b2fbd7e23c4b56d51f1065ce0b552b1
adam1025 <adam@quantaser.com>
2020-06-18 15:44:39 +0800

    d

--- MST_v3.c
+++ MST_v3.c
@@ -136,6 +136,7 @@ int main(int argc, char *argv[])
 		fprintf(stderr, "Rp api init failed!\n");
 	}
 	map2virtualAddr(&adc_idx_addr, 0x40200064); //adc_idx
+	//0x40200068 for ch1, 0x40200070 for ch2
 	map2virtualAddr(&adc_ch2, 0x40200070); //adc_ch2
 	
 	i2cOpen();
