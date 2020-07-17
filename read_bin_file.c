6884cd66ab55d3fa466cb6eab1ddc6e7133cc516
adam1025 <adam@quantaser.com>
2020-07-17 14:54:30 +0800

    d

--- read_bin_file.c
+++ read_bin_file.c
@@ -38,11 +38,12 @@ int main(int argc, char *argv[])
 		// printf("%d. %f\n", i, arr[i]);
 	// }
 	fclose(fp);
+	arr = arr/1000;
 	
 	rp_GenAmp(RP_CH_2, 0);
 	rp_GenOutEnable(RP_CH_2);
 	rp_GenWaveform(RP_CH_2, RP_WAVEFORM_ARBITRARY);
-	rp_GenArbWaveform(RP_CH_2, arr/1000, arb_size);
+	rp_GenArbWaveform(RP_CH_2, arr, arb_size);
 	rp_GenFreq(RP_CH_2, 1000.0/t);
 	
 	rp_GenAmp(RP_CH_2, 1); // chirp start
