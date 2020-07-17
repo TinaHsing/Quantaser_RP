39b234a72184ac7f723876862985cfa27a35f071
adam1025 <adam@quantaser.com>
2020-07-17 14:42:00 +0800

    d

--- read_bin_file.c
+++ read_bin_file.c
@@ -25,25 +25,22 @@ int main(int argc, char *argv[])
 	FILE *fp;
 	long arb_size = 32768;
 	float arr[arb_size];
-	int t;
+	// int t;
 	
-	t = atoi(argv[1]);
-	printf("0");
-	fp = fopen(argv[2], "rb");
-	printf("1");
+	// t = atoi(argv[1]);
+	fp = fopen(argv[1], "rb");
 	fread(arr, sizeof(float), arb_size, fp);
-	printf("2");
-	// for(int i=0; i<arb_size; i++) {
-		// printf("%d. %f\n", i, arr[i]);
-	// }
-	fclose(fp);
+	for(int i=0; i<arb_size; i++) {
+		printf("%d. %f\n", i, arr[i]);
+	}
+	// fclose(fp);
 	
-	rp_GenAmp(RP_CH_2, 0);
-	rp_GenWaveform(RP_CH_2, RP_WAVEFORM_ARBITRARY);
-	rp_GenArbWaveform(RP_CH_2, arr, arb_size);
-	rp_GenFreq(RP_CH_2, 1000.0/t);
+	// rp_GenAmp(RP_CH_2, 0);
+	// rp_GenWaveform(RP_CH_2, RP_WAVEFORM_ARBITRARY);
+	// rp_GenArbWaveform(RP_CH_2, arr, arb_size);
+	// rp_GenFreq(RP_CH_2, 1000.0/t);
 	
-	rp_GenAmp(RP_CH_2, 1); // chirp start
-	usleep(t*1000);
-	rp_GenAmp(RP_CH_2, 0); //chirp end
+	// rp_GenAmp(RP_CH_2, 1); // chirp start
+	// usleep(t*1000);
+	// rp_GenAmp(RP_CH_2, 0); //chirp end
 }
