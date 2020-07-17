31c0154dae0524937e6babdd6a77353cb82fa109
adam1025 <adam@quantaser.com>
2020-07-17 14:31:48 +0800

    d

--- read_bin_file.c
+++ read_bin_file.c
@@ -1,16 +1,46 @@
+
+//////*header include*//////
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <sys/time.h>
+#include <sys/ioctl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <linux/ioctl.h>
+#include <linux/i2c-dev.h>
+#include <stdint.h>
+#include <unistd.h>
+#include <fcntl.h> 
+#include <termios.h> 
+#include <errno.h>
+#include <math.h>
+#include <signal.h>
+#include <ctype.h>
+#include <sys/mman.h>
+#include "redpitaya/rp.h"
 
 int main(int argc, char *argv[])
 {
 	FILE *fp;
-	size_t cnt = atoi(argv[2]);
-	double arr[50000];
+	long arb_size = 32768
+	float arr[50000];
+	int t;
 	
-	fp = fopen(argv[1], "rb");
-	fread(arr, sizeof(double), cnt, fp);
-	for(int i=0; i<cnt; i++) {
-		printf("%d. %f\n", i, arr[i]);
-	}
+	t = atoi(argv[1]);
+	fp = fopen(argv[2], "rb");
+	fread(arr, sizeof(float), arb_size, fp);
+	// for(int i=0; i<arb_size; i++) {
+		// printf("%d. %f\n", i, arr[i]);
+	// }
 	fclose(fp);
+	
+	rp_GenAmp(RP_CH_2, 0);
+	rp_GenWaveform(RP_CH_2, RP_WAVEFORM_ARBITRARY);
+	rp_GenArbWaveform(RP_CH_2, arr, arb_size);
+	rp_GenFreq(RP_CH_2, 1000.0/CHIRP_SWEEP_TIME);
+	
+	rp_GenAmp(RP_CH_2, chirp_amp); // chirp start
+	usleep(t*1000);
+	rp_GenAmp(RP_CH_2, 0); //chirp end
 }
